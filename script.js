5c0552fe77375231c39781f50f89180d6bd10985
Tiffany Patton <tiffany.zhou.7@gmail.com>
2022-06-27 18:16:57 -0400

    Fetch addition

--- script.js
+++ script.js
@@ -68,14 +68,21 @@ window.addEventListener("load", function () {
         }
     });
 
-    let listedPlanets;
-    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-    let listedPlanetsResponse = myFetch();
-    listedPlanetsResponse.then(function (result) {
-        listedPlanets = result;
-        console.log(listedPlanets);
-    }).then(function () {
-        console.log(listedPlanets);
-        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-    })
+    // let listedPlanets;
+    // // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+    // let listedPlanetsResponse = myFetch();
+    // listedPlanetsResponse.then(function (result) {
+    //     listedPlanets = result;
+    //     console.log(listedPlanets);
+    // }).then(function () {
+    //     console.log(listedPlanets);
+    //     // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+    // })
+
+    fetch("https://handlers.education.launchcode.org/static/planets.json").then(function (response) {
+        // Access the JSON in the response
+        response.json().then(function (json) {
+            console.log(json);
+        });
+    });
 });
